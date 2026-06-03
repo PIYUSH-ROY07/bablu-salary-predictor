53162faee067f3899573442423bce6c950dc7990
PIYUSH-ROY07 <piyush.royy07@gmail.com>
2026-06-03 06:26:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -331,7 +331,6 @@ header_col, lottie_col = st.columns([3, 1])
 with header_col:
     st.title("Bablu Salary Predictor")
     st.caption("Enter your profile details below to get a market-aligned salary prediction.")
-with lottie_col
 
 left_col, right_col = st.columns(2)
 
